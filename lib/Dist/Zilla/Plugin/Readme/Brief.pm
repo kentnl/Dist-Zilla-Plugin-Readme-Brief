41144f68ca8d6dcd741a45578af4badff8b30e2e
Kent Fredric <kentfredric@gmail.com>
2015-01-06 08:52:24 +1300

    Build results of 4d7bc76 (on master)

--- lib/Dist/Zilla/Plugin/Readme/Brief.pm
+++ lib/Dist/Zilla/Plugin/Readme/Brief.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Readme::Brief;
 
-our $VERSION = '0.002000';
+our $VERSION = '0.002001';
 
 # ABSTRACT: Provide a short simple README with just the essentials
 
@@ -329,7 +329,7 @@ Dist::Zilla::Plugin::Readme::Brief - Provide a short simple README with just the
 
 =head1 VERSION
 
-version 0.002000
+version 0.002001
 
 =head1 SYNOPSIS
 
