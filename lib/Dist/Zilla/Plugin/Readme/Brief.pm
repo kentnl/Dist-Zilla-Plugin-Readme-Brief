30aef4b06fdfc5a6777ba3cef5e21072489bfae9
Kent Fredric <kentfredric@gmail.com>
2015-01-06 18:14:06 +1300

    v0.002001
     [Documentation]
     - Fix up outdated references/properly reference new attribute.
     - More minor spelling mistakes
     - Tidier whitespace (aristotle++, #2)
    
     [Generation]
     - Improved README text copywriting (aristotle++, #2)
    
     [Internals]
     - Improved structuring (aristotle++, #2)

--- lib/Dist/Zilla/Plugin/Readme/Brief.pm
+++ lib/Dist/Zilla/Plugin/Readme/Brief.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Readme::Brief;
 
-our $VERSION = '0.002001';
+our $VERSION = '0.002002';
 
 # ABSTRACT: Provide a short simple README with just the essentials
 
