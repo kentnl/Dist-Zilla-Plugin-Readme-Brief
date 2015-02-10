2b02bcfb9591bdc839e531eba63a7dccc607698f
Kent Fredric <kentfredric@gmail.com>
2015-02-10 18:42:50 +1300

    Build results of b6bd32a (on master)

--- lib/Dist/Zilla/Plugin/Readme/Brief.pm
+++ lib/Dist/Zilla/Plugin/Readme/Brief.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Readme::Brief;
 
-our $VERSION = '0.002004';
+our $VERSION = '0.002005';
 
 # ABSTRACT: Provide a short simple README with just the essentials
 
@@ -305,7 +305,7 @@ Dist::Zilla::Plugin::Readme::Brief - Provide a short simple README with just the
 
 =head1 VERSION
 
-version 0.002004
+version 0.002005
 
 =head1 SYNOPSIS
 
