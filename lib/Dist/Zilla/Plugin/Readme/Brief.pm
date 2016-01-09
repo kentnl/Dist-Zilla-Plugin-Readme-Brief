d3e8b8b0d8978cf96796b6f1a6696d37c4800266
Kent Fredric <kentfredric@gmail.com>
2016-01-10 03:44:24 +1300

    Set V=0.003000 due to major features

--- lib/Dist/Zilla/Plugin/Readme/Brief.pm
+++ lib/Dist/Zilla/Plugin/Readme/Brief.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Readme::Brief;
 
-our $VERSION = '0.002006';
+our $VERSION = '0.003000';
 
 # ABSTRACT: Provide a short simple README with just the essentials
 
