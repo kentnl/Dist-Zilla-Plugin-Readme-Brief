34bd9bd4e9055b63dfd330c2a023feffb05aca68
Kent Fredric <kentfredric@gmail.com>
2017-03-02 13:05:44 +1300

    v0.003001
     [Bugfix]
     - Avoid test failures on 5.26 w/ -Ddefault_inc_excludes_dot
     - Closes https://github.com/kentnl/Dist-Zilla-Plugin-Readme-Brief/issues/6
    
     [Dependencies::Stats]
     - Dependencies changed since 0.003000, see misc/*.deps* for details
     - develop: +1 ↑1 (suggests: ↑1)
     - test: ↓1

--- lib/Dist/Zilla/Plugin/Readme/Brief.pm
+++ lib/Dist/Zilla/Plugin/Readme/Brief.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Readme::Brief;
 
-our $VERSION = '0.003001';
+our $VERSION = '0.003002';
 
 # ABSTRACT: Provide a short simple README with just the essentials
 
